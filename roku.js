f9e9cad60c63f3edf059e8de59714f50c58dc27e
Tyson <onaclov2000+github@gmail.com>
2013-02-10 03:06:56 -0700

    Added SSDP data, still needs more work though.

--- roku.js
+++ roku.js
@@ -5,7 +5,47 @@
 //       I'm sure there is more, also some commands will be dependent on the Roku version, so something to check for that, using
 //       the returned SSDP would be wise.
 var http = require('http')
-var ip = '192.168.1.5'
+var dgram = require('dgram'); // dgram is UDP
+var ip = '' 
+// Listen for responses
+function listen(port) {
+	var server = dgram.createSocket("udp4");
+ 
+	server.on("message", function (msg, rinfo) {
+		console.log("server got: " + msg + " from " + rinfo.address + ":" + rinfo.port);
+                ip = msg;
+	});
+	server.bind(port); // Bind to the random port we were given when sending the message, not 1900
+ 
+	// Give it a while for responses to come in
+	setTimeout(function(){
+		console.log("Finished waiting");
+		server.close();
+	},20000);
+}
+ 
+function search() {
+	
+	var message = new Buffer(
+		"M-SEARCH * HTTP/1.1\r\n" +
+		"Host:239.255.255.250:1900\r\n" +
+		"Man:\"ssdp:discover\"\r\n" +
+		"ST: roku:ecp\r\n" + // Essential, used by the client to specify what they want to discover, eg 'ST:ge:fridge'
+		"MX:2\r\n" + // 1 second to respond (but they all respond immediately?)
+		"\r\n"
+	);
+ 
+	var client = dgram.createSocket("udp4");
+	client.bind(1900, "239.255.255.250"); // So that we get a port so we can listen before sending
+	listen(client.address().port);
+	client.send(message, 0, message.length, 1900, "239.255.255.250", function(err, bytes) {
+  client.close();});
+}
+ 
+search();
+
+
+
 var apps = {
   hostname: ip,
   port: 8060,
